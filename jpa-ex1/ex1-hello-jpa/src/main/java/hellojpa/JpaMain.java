e65967bfaaeb246ba93059c87d9491bd74553fb4
최진석 <cj5255@naver.com>
2023-07-14 12:00:24 +0900

    9-2 finish

--- jpa-ex1/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa-ex1/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -23,21 +23,11 @@ public class JpaMain {
 
         try{
 
-            Child child1 = new Child();
-            Child child2 = new Child();
-
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
-
-            em.persist(parent);
-
-            em.flush();
-            em.clear();
-
-            Parent findParent = em.find(Parent.class, parent.getId());
-            findParent.getChildList().remove(0);
-
+            Member member = new Member();
+            member.setUsername("hello");
+            member.setHomeAddress(new Address("city", "street", "1000"));
+            member.setWorkPeriod(new Period());
+            em.persist(member);
 
 
             tx.commit();
@@ -50,23 +40,23 @@ public class JpaMain {
         emf.close();
     }
 
-    private static void logic(Member m1, Member m2){
-//        System.out.println("m1 == m2: " + (m1.getClass() == m2.getClass()));
-        System.out.println("m1 == m2: " + (m1 instanceof Member));
-        System.out.println("m1 == m2: " + (m2 instanceof Member));
+//    private static void logic(Member m1, Member m2){
+////        System.out.println("m1 == m2: " + (m1.getClass() == m2.getClass()));
+//        System.out.println("m1 == m2: " + (m1 instanceof Member));
+//        System.out.println("m1 == m2: " + (m2 instanceof Member));
+//
+//
+//    }
 
-
-    }
-
-    private static void printMember(Member member) {
-        System.out.println("member = " + member.getUsername());
-    }
-
-    private static void printMemberAndTeam(Member member) {
-        String username = member.getUsername();
-        System.out.println("username = " + username);
-
-        Team team = member.getTeam();
-        System.out.println("team = " + team.getName());
-    }
+//    private static void printMember(Member member) {
+//        System.out.println("member = " + member.getUsername());
+//    }
+//
+//    private static void printMemberAndTeam(Member member) {
+//        String username = member.getUsername();
+//        System.out.println("username = " + username);
+//
+//        Team team = member.getTeam();
+//        System.out.println("team = " + team.getName());
+//    }
 }
--- jpa-ex1/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa-ex1/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,33 +1,38 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Member extends BaseEntity{
-    @Id @GeneratedValue
+public class Member{
+    @Id
+    @GeneratedValue
     @Column(name="MEMBER_ID")
     private Long id;
 
     @Column(name="USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn
-    private Team team;
+    //기간 Period
+    @Embedded
+    private Period workPeriod;
 
-    @OneToOne
-    @JoinColumn(name="LOCKER_ID")
-    private Locker locker;
+   // 주소
+    @Embedded
+    private Address homeAddress;
 
-//    @ManyToMany
-//    @JoinTable(name = "MEMBER_PRODUCT")
-//    private List<Product> products = new ArrayList<>();
-
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
-
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name="city",
+                            column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name="street",
+                    column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name="zipcode",
+                    column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
 
     public Long getId() {
         return id;
@@ -45,11 +50,19 @@ public class Member extends BaseEntity{
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
+    public Period getWorkPeriod() {
+        return workPeriod;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
     }
 }
