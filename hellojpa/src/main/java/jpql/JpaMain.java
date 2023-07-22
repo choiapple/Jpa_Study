80075f7b16b19250b9a51c189516348d97a02ff1
choiapple <79374648+choiapple@users.noreply.github.com>
2023-07-23 01:36:08 +0900

    11-5 finish

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -19,27 +19,38 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-            member.setTeam(team);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
 
             em.flush();
             em.clear();
 
-            String query  = "select function('group_concat', m.username) From Member m";
-            List<String> result = em.createQuery(query, String.class)
-                            .getResultList();
-            for(String s : result){
-                System.out.println("s = " + s);
-            }
+            String query  = "select m from Member m where m = :member";
+            Member findMember = em.createQuery(query, Member.class)
+                            .setParameter("member",member1)
+                            .getSingleResult();
+            System.out.println("findMember = " + findMember);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
